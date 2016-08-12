d58df2a9de659b5e1e7571be4a47017108b02d8d
Anton Herzog <info@antonherzog.com>
2016-08-12 17:08:08 +0200

    Added test.

--- FibonacciHeap.Tests/FibonacciHeapFacts.cs
+++ FibonacciHeap.Tests/FibonacciHeapFacts.cs
@@ -10,6 +10,7 @@ namespace FibonacciHeap.Tests
 {
     public class FibonacciHeapFacts
     {
+        private List<FibonacciHeapNode<int>> _addedNodes = new List<FibonacciHeapNode<int>>();
         private FibonacciHeapNode<int> _node;
         private FibonacciHeap<int> _sut;
 
@@ -27,9 +28,25 @@ namespace FibonacciHeap.Tests
             _sut.Insert(_node);
         }
 
-        private void GivenNode()
+        [Fact]
+        public void It_can_order_two_nodes_correctly()
         {
-            _node = new FibonacciHeapNode<int>(0, 0);
+            WhenSutIsCreated();
+            WhenNodeIsAdded(0,0);
+            WhenNodeIsAdded(2,5);
+            Assert.Equal(_addedNodes[0],_sut.Min());
+        }
+
+        private void WhenNodeIsAdded(int data, int key)
+        {
+            GivenNode(data,key);
+            _sut.Insert(_node);
+            _addedNodes.Add(_node);
+        }
+
+        private void GivenNode(int data=0, int key=0)
+        {
+            _node = new FibonacciHeapNode<int>(data, key);
         }
 
         private void WhenSutIsCreated()
